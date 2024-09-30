9997d47f1de3d3892a5abe0c9cf3a12d9e561333
Oluwatobi Olubode <oluwatobiolubode2017@gmail.com>
2024-09-29 19:08:35 -0700

    my project is a success

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFzty915IF_pr4afI15wEOld_UWbMxLEY",
-  authDomain: "my-instagram-clone-163e3.firebaseapp.com",
-  projectId: "my-instagram-clone-163e3",
-  storageBucket: "my-instagram-clone-163e3.appspot.com",
-  messagingSenderId: "307166712160",
-  appId: "1:307166712160:web:91f3cbd3140157aa2ace05",
-  measurementId: "G-WEYRZNRT8K"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_API_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREEMENT,
 };
 
 // Initialize Firebase
