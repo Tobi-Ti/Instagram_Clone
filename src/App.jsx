e4d0fb72e7dc34df19d80d5a9ab463a2e25c863d
Oluwatobi Olubode <oluwatobiolubode2017@gmail.com>
2024-09-29 19:28:10 -0700

    My Instagram Clone

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import PageLayout from "./Layouts/PageLayout/PageLayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase/firebase";
-import ProfilePage from "./Pages/ProfilePage/ProfilePage";
-
 
 function App() {
 	const [authUser] = useAuthState(auth);
